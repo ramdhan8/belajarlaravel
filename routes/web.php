c1ee1955cfa3fbf841b7b3186d61442ef4db4a32
ramdhan8 <nurrahmatramdhan8@gmail.com>
2024-02-22 07:42:45 +0700

    simple

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('blog', function () {
+    return view('blog');
+});
+Route::get('belajar', function () {
+    return view('belajar');
+});
